837221c689a1028ab23a08653a0fb7a053ff311f
anna-sahradyan <a.sahradyan72@gmail.com>
2022-11-03 21:43:26 +0400

    create ul

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+
+const App = () => {
+    return (
+        <>
+            <div className={"wrapper"}>
+                <div className={"main"}>main
+
+                </div>
+                <div className={"pyramid"}>
+                    <ul className={"moneyList"}>
+                        <li className={"moneyListItem"}>
+                            <span className={"moneyListItemNumber"}>4</span>
+                            <span className={"moneyListItemAmount"}>$ 400</span>
+                            </li>
+                        <li className={"moneyListItem"}>
+                            <span className={"moneyListItemNumber"}>4</span>
+                            <span className={"moneyListItemAmount"}>$ 400</span>
+                        </li> <li className={"moneyListItem"}>
+                        <span className={"moneyListItemNumber"}>4</span>
+                        <span className={"moneyListItemAmount"}>$ 400</span>
+                    </li> <li className={"moneyListItem"}>
+                        <span className={"moneyListItemNumber"}>4</span>
+                        <span className={"moneyListItemAmount"}>$ 400</span>
+                    </li> <li className={"moneyListItem"}>
+                        <span className={"moneyListItemNumber"}>4</span>
+                        <span className={"moneyListItemAmount"}>$ 400</span>
+                    </li> <li className={"moneyListItem"}>
+                        <span className={"moneyListItemNumber"}>4</span>
+                        <span className={"moneyListItemAmount"}>$ 400</span>
+                    </li> <li className={"moneyListItem"}>
+                        <span className={"moneyListItemNumber"}>4</span>
+                        <span className={"moneyListItemAmount"}>$ 400</span>
+                    </li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </>
+    );
+};
+
+export default App;
